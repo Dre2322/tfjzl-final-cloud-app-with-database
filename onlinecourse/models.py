01952aef51fc37e91c073c5c50410b2eff3f8ae6
Dre2322 <2dre34@gmail.com>
2026-04-08 14:25:28 -0500

    Added Question, Choice, and Submission models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -124,7 +124,9 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-# One enrollment could have multiple submission One submission could have multiple choices One choice could belong to multiple submissions
+# One enrollment could have multiple submission 
+# One submission could have multiple choices
+# One choice could belong to multiple submissions
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
